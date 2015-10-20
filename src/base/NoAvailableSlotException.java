d87dc4423291846da7e3f3ea4dfcbbc5f8cbcc3d
destrucules <destrucules@gmail.com>
2015-10-20 19:45:37 -0400

    -Added new NoAvailableSlotException for when the probe fails -Wrote the probe - linear -Changed FileArray to contain get and set methods -Implemented FileArray get and set method use in compress() -Added a new variable "unitSize" and changed "chunkSize" to have a more logical meaning -Added chunk size control to compress() -Added a currentSlot variable to label the part of the file currently being acted upon -Wrote the place() method -Added an error reporting feature to the place() method -Fleshed out implicate() method - should be finished now -Added value() helper method for implicate() -Added end() getter to FileArray

---
+++ src/base/NoAvailableSlotException.java
@@ -0,0 +1,6 @@
+package base;
+
+public class NoAvailableSlotException extends Exception {
+	private static final long serialVersionUID = 1L;
+	//TODO The whole class - what should I put here to "pimp out" the exception?
+}
